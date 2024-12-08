89bc307b26587d30990eaf5111f50e77370563b2
1152079 <aehcf01@inbox.ru>
2024-12-08 12:46:08 +0300

    Update app

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-print("Lab 8: Deploy by Jenkins")
-print("Задача: задеплоить личный проект с GitHub с помощью Jenkins.")
+from os import name
+
+from flask import Flask
+
+app = Flask(name)
+
+@app.route("/")
+def home():
+    return ("<h1>Lab 8: Deploy by Jenkins"
+            "Задача: задеплоить личный проект с GitHub с помощью Jenkins.</p>")
+
+if name == "main":
+    app.run(debug=True)
